63e3248c0aa52a6d4290a8877fa708f30e715a07
JustinWeq <zoomdude111@hotmail.com>
2016-03-08 14:27:20 -0800

    Initial commit

---
+++ app/src/main/java/cs241/virtualaquariumdemo/MainActivity.java
@@ -0,0 +1,49 @@
+package cs241.virtualaquariumdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+
+/**
+ * This class handles the main activity's layout
+ */
+public class MainActivity extends FragmentActivity {
+
+    /**
+     * called upon creation of the activity
+     * @param savedInstanceState
+     *///l
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * called when the orca button is clicked on
+     * @param view the buttons view
+     */
+    public void onClickWhale(View view)
+    {
+
+    }
+
+    /*******
+    /**
+     * called when the salmon button is clicked on
+     * @param view the buttons view
+     */
+    public void onClickSalmon(View view)
+    {
+
+    }
+
+    /**
+     * called when the otter button is clicked on
+     * @param view the buttons view
+     */
+    public void onClickOtter(View view)
+    {
+
+    }
+}
